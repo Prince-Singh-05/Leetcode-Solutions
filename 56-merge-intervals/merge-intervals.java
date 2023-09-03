6c9af38826ddad583ec47fbf900285ea43cb3038
Prince Singh <83657758+Luciefur@users.noreply.github.com>
2023-09-03 23:38:36 +0530

    Time: 8 ms (94.02%) | Memory: 45.5 MB (50.72%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,17 +1,46 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
+    // public int[][] merge(int[][] intervals) {
+
+    //     Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+    //     List<int[]> ans = new ArrayList<>();
         
-        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-        List<int[]> ans = new ArrayList<>();
+    //     for (int[] interval : intervals) {
+    //         if (ans.isEmpty() || interval[0] > ans.get(ans.size() - 1)[1]) {
+    //             ans.add(interval);
+    //         } else {
+    //             ans.get(ans.size() - 1)[1] = Math.max(interval[1], ans.get(ans.size() - 1)[1]);
+    //         }
+    //     }
         
-        for (int[] interval : intervals) {
-            if (ans.isEmpty() || interval[0] > ans.get(ans.size() - 1)[1]) {
-                ans.add(interval);
-            } else {
-                ans.get(ans.size() - 1)[1] = Math.max(interval[1], ans.get(ans.size() - 1)[1]);
+    //     return ans.toArray(new int[ans.size()][]);
+    // }
+
+    public int[][] merge(int[][] A) {
+        Arrays.sort(A, (a,b)-> a[0]-b[0]);
+        int n = A.length;
+        for(int i=1;i<A.length;i++){
+            if(A[i][0] <= A[i-1][1]){
+                A[i][1] = Math.max(A[i-1][1],A[i][1]);
+                A[i][0] = A[i-1][0];
+                A[i-1][0] = -1;
+                A[i-1][1] = -1;n--;
             }
         }
         
-        return ans.toArray(new int[ans.size()][]);
+        int[][] ans = new int[n][2];
+        
+        int i=0;
+	
+        for(int[] a : A){
+            if(a[0]!= -1){
+                ans[i][0] = a[0];
+                ans[i][1] = a[1];
+                i++;
+            }
+        }
+        
+        return ans;
+        
     }
+
 }
