d00b8f6c695f15ac3561e7a2a82315e1efc35737
Prince Singh <83657758+Luciefur@users.noreply.github.com>
2023-09-11 23:28:15 +0530

    Time: 6 ms (14.70%) | Memory: 41 MB (85.41%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -2,19 +2,39 @@ import java.util.*;
 
 class Solution {
 
-    public List<String> ans = new ArrayList<>();
+    // public List<String> ans = new ArrayList<>();
 
-    public void giveComb(String digits, String arr[], int i, String comb) {
-        if(i == digits.length()) {
-            ans.add(comb);
-            return;
+    // public void giveComb(String digits, String arr[], int i, String comb) {
+    //     if(i == digits.length()) {
+    //         ans.add(comb);
+    //         return;
+    //     }
+
+    //     char c = digits.charAt(i);
+    //     String a = arr[c - '0'];
+    //     for(char x1 : a.toCharArray()) {
+    //         giveComb(digits, arr, i+1, comb+x1);
+    //     }
+    // }
+
+    private List<String> giveComb(String digits, String[] arr) {
+        if (digits.length() == 0) {
+            List<String> bs = new ArrayList<>();
+            bs.add("");
+            return bs;
         }
 
-        char c = digits.charAt(i);
+        char c = digits.charAt(0);
         String a = arr[c - '0'];
-        for(char x1 : a.toCharArray()) {
-            giveComb(digits, arr, i+1, comb+x1);
+        String smallinput = digits.substring(1);
+        List<String> rest = giveComb(smallinput, arr);
+        List<String> res = new ArrayList<>();
+        for (String x : rest) {
+            for (char x1 : a.toCharArray()) {
+                res.add(x1 + x);
+            }
         }
+        return res;
     }
 
     public List<String> letterCombinations(String digits) {
@@ -26,8 +46,9 @@ class Solution {
         }
 
         String arr[] = {"0", "0", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        giveComb(digits, arr, 0, "");
-        return ans;
+        // giveComb(digits, arr, 0, "");
+        res = giveComb(digits, arr);
+        return res;
         
     }
 }
