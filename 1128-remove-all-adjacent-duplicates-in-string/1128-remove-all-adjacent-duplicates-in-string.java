56bb53d4ff832d50020a811571d31a38fb6c9097
Prince Singh <83657758+Luciefur@users.noreply.github.com>
2023-09-30 13:05:23 +0530

    Time: 24 ms (66.43%), Space: 43.8 MB (95.54%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -1,15 +1,32 @@
 class Solution {
     public String removeDuplicates(String s) {
-        String ans = "";
+        
+        // String ans = "";
+
+        // for(int i = 0; i<s.length(); i++) {
+        //     if(ans.isEmpty() || ans.charAt(ans.length()-1) != s.charAt(i)) {
+        //         ans += s.charAt(i);
+        //     } else {
+        //         ans = ans.substring(0, ans.length()-1);
+        //     }
+        // }
+
+        Stack<Character> st = new Stack<>();
 
         for(int i = 0; i<s.length(); i++) {
-            if(ans.isEmpty() || ans.charAt(ans.length()-1) != s.charAt(i)) {
-                ans += s.charAt(i);
+            if(st.isEmpty() || st.peek() != s.charAt(i)) {
+                st.push(s.charAt(i));
             } else {
-                ans = ans.substring(0, ans.length()-1);
+                st.pop();
             }
         }
 
-        return ans;
+        StringBuilder ans = new StringBuilder();
+
+        for(char c : st) {
+            ans.append(c);
+        }
+
+        return ans.toString();
     }
 }
