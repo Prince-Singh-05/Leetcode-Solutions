2a9b5a4f477af76282fddffbb0fb9343dbcf04ff
Prince Singh <83657758+Luciefur@users.noreply.github.com>
2023-10-03 22:05:44 +0530

    Time: 0 ms (100%), Space: 40.7 MB (7.26%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.java
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.java
@@ -1,16 +1,27 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
         int n = nums.length;
-        int count = 0;
+        
+        // O(n^2)
+        // int count = 0;
 
-        for(int i = 0; i<n-1; i++) {
-            for(int j = i+1; j<n; j++) {
-                if(nums[i] == nums[j]) {
-                    count++;
-                }
-            }
+        // for(int i = 0; i<n-1; i++) {
+        //     for(int j = i+1; j<n; j++) {
+        //         if(nums[i] == nums[j]) {
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        // O(N)
+
+        int ans = 0;
+        int count[] = new int[101];
+
+        for(int i : nums) {
+            ans += count[i]++;
         }
 
-        return count;
+        return ans;
     }
 }
