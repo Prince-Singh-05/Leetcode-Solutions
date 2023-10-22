5d1d3e31a36e38876ac447e018ecf8ad4b381803
Prince Singh <83657758+Luciefur@users.noreply.github.com>
2023-10-22 23:11:13 +0530

    Time: 0 ms (100%), Space: 10 MB (24.75%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -3,6 +3,9 @@ public:
     int findKthPositive(vector<int>& arr, int k) {
         
         int n = arr.size();
+
+        // T.C. -> O(N)
+        /*
         for(int i = 0; i<n; i++) {
             if(arr[i] > k) {
                 break;
@@ -10,7 +13,24 @@ public:
 
             k++;
         }
-
         return k;
+        */
+
+        // Binary Search TC -> O(log N)
+
+        int low = 0, high = n-1;
+
+        while(low <= high) {
+            int mid = (low+high)/2;
+            int missing = arr[mid] - (mid+1);
+
+            if(missing < k) {
+                low = mid+1;
+            } else {
+                high = mid-1;
+            } 
+        }
+
+        return (low + k);
     }
 };
