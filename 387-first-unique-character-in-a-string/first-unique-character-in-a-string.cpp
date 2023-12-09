84f25724004b9ab9b865f38872cd03e8e60dd78f
Prince Singh <83657758+Luciefur@users.noreply.github.com>
2023-12-09 21:52:02 +0530

    Time: 29 ms (66.06%) | Memory: 11.1 MB (34.13%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,30 +1,45 @@
 class Solution {
 public:
-    int firstUniqChar(string s) {
+    // int firstUniqChar(string s) {
 
-        queue<int> q;
-        int freq[26] = {0};
+    //     queue<int> q;
+    //     int freq[26] = {0};
+
+    //     for(int i = 0; i<s.length(); i++) {
+    //         char ch = s[i];
+
+    //         freq[ch - 'a']++;
+    //         q.push(i);
+
+
+    //         while(!q.empty()) {
+
+    //             char frontCharacter = s[q.front()];
+    //             if(freq[frontCharacter-'a'] > 1) {
+    //                 q.pop();
+    //             } else break;
+    //         }
+    //     }
+        
+    //     if(q.empty()) {
+    //         return -1;
+    //     }
+    //     return q.front();
+
+    // }
+
+    int firstUniqChar(string s) {
+        unordered_map<char, int> mp;
 
         for(int i = 0; i<s.length(); i++) {
-            char ch = s[i];
-
-            freq[ch - 'a']++;
-            q.push(i);
-
-
-            while(!q.empty()) {
-
-                char frontCharacter = s[q.front()];
-                if(freq[frontCharacter-'a'] > 1) {
-                    q.pop();
-                } else break;
-            }
+            mp[s[i]]++;
         }
-        
-        if(q.empty()) {
-            return -1;
-        }
-        return q.front();
 
+        for(int i = 0; i<s.length(); i++) {
+            if(mp[s[i]] == 1) 
+                return i;
+        }
+
+        return -1;
     }
 };
