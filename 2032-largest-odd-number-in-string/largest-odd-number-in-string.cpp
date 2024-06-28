1af0183e02c891a98635d84e338fed57648507ad
Prince Singh <83657758+Luciefur@users.noreply.github.com>
2024-06-28 12:51:52 +0530

    Time: 25 ms (78.54%) | Memory: 16.1 MB (84.19%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,24 +1,38 @@
+// class Solution {
+// public:
+//     string largestOddNumber(string num) {
+
+//         string ans;
+//         int end = -1;  
+
+//         for(int i = num.size()-1; i >= 0; i--) {
+
+//             if((num[i] - '0')%2 != 0) {
+//                 end = i;
+//                 break;
+//             }
+               
+//         }
+ 
+
+//         for(int i = 0; i<=end; i++) {
+//             ans += num[i];
+//         }
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     string largestOddNumber(string num) {
-
-        string ans;
-        int end = -1;  
-
-        for(int i = num.size()-1; i >= 0; i--) {
-
-            if((num[i] - '0')%2 != 0) {
-                end = i;
-                break;
+        for (int i=num.length()-1; i>=0; i--){
+            int digit = num[i]-'0' ;
+            if (digit & 1){
+                return num.substr(0, i+1) ;
             }
-               
         }
- 
-
-        for(int i = 0; i<=end; i++) {
-            ans += num[i];
-        }
-
-        return ans;
+        return "" ;
     }
 };
