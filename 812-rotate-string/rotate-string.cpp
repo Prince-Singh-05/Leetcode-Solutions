eda1f6ad76a948dfa9b5cedc76de3289fd52674d
Prince Singh <83657758+Luciefur@users.noreply.github.com>
2024-07-01 16:24:03 +0530

    Time: 3 ms (47.98%) | Memory: 7.4 MB (62.79%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,10 +3,30 @@ public:
     bool rotateString(string s, string goal) {
 
         if(s.size() != goal.size()) return false;
-
-        string concatenated = s + s;
-
-        return concatenated.find(goal) != string::npos;
+        if(s.size() == 0 && goal.size() == 0) return true;
+        
+        vector<int> CC_class;
+        
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == goal[0]){
+                CC_class.push_back(i);
+            }
+        }
+        
+        for(int start : CC_class){
+            bool isRotate = true;
+            for(int i = 0; i < s.size(); i++){
+                if(s[(start+i)%s.size()] != goal[i]){
+                    isRotate = false;
+                    break;
+                }
+            }
+            if(isRotate){
+                return true;
+            }
+        }
+        
+        return false;
         
     }
 };
