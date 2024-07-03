4cdf5ce27c44a2c057b45a323df27c7567405188
Prince Singh <83657758+Luciefur@users.noreply.github.com>
2024-07-03 10:04:10 +0530

    Time: 547 ms (5.06%) | Memory: 239 MB (5.18%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    char largestFreq(unordered_map<char, int> freq) {
+
+        char key = '/';
+        int maxValue = INT_MIN;
+
+        for(auto& pair : freq) {
+            if(pair.second > maxValue) {
+                key = pair.first;
+                maxValue = pair.second;
+            }
+        }
+
+        return key;
+    }
+
+    string frequencySort(string s) {
+
+        unordered_map<char, int> freq;
+
+        for(char c : s) {
+            freq[c]++;
+        }
+
+        // sort(freq.begin(), freq.end());
+
+        string ans = "";
+
+        for(int i = 0; i<s.size(); i++) {
+            char c = largestFreq(freq);
+            
+            while(freq[c] > 0) {
+                ans += c;
+                freq[c]--;
+            }
+        }
+
+        return ans;
+        
+    }
+};
