5d42022300f422facd5cc1a09ff52923f5f9ab97
Prince Singh <83657758+Luciefur@users.noreply.github.com>
2024-07-03 10:08:16 +0530

    Time: 10 ms (63.58%) | Memory: 10.8 MB (53.86%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -16,26 +16,54 @@ public:
         return key;
     }
 
-    string frequencySort(string s) {
+    // string frequencySort(string s) {
 
+    //     unordered_map<char, int> freq;
+
+    //     for(char c : s) {
+    //         freq[c]++;
+    //     }
+
+    //     string ans = "";
+
+    //     for(int i = 0; i<s.size(); i++) {
+    //         char c = largestFreq(freq);
+            
+    //         while(freq[c] > 0) {
+    //             ans += c;
+    //             freq[c]--;
+    //         }
+    //     }
+
+    //     return ans;
+        
+    // }
+
+    string frequencySort(string s) {
         unordered_map<char, int> freq;
 
-        for(char c : s) {
+        // Count character frequencies
+        for (char c : s) {
             freq[c]++;
         }
 
-        string ans = "";
+        // Create a min heap (priority queue) based on frequency
+        priority_queue<pair<int, char>> pq;
+        for (const auto& pair : freq) {
+            pq.push({pair.second, pair.first});
+        }
 
-        for(int i = 0; i<s.size(); i++) {
-            char c = largestFreq(freq);
-            
-            while(freq[c] > 0) {
+        // Build the sorted string
+        string ans = "";
+        while (!pq.empty()) {
+            int count = pq.top().first;
+            char c = pq.top().second;
+            pq.pop();
+            while (count--) {
                 ans += c;
-                freq[c]--;
             }
         }
 
         return ans;
-        
     }
 };
