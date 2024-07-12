61f27bac731b29354b6e2677f7718ba6d38b5b3d
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2024-07-12 23:52:08 +0530

    Time: 6 ms (35.38%) | Memory: 16.7 MB (6.63%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -2,11 +2,35 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
 
-        sort(nums.begin(), nums.end());
+        // T.C => O(NlogN)
 
+        // sort(nums.begin(), nums.end());
+
+        // int n = nums.size();
+        // int l = 0;
+        // int r = n-1;
+
+        // while(l <= r) {
+
+        //     int mid = (r-l)/2 + l;
+
+        //     if(nums[mid] == target) {
+        //         return true;
+        //     } 
+        //     else if(nums[mid] < target) {
+        //         l = mid+1;
+        //     } else {
+        //         r = mid-1;
+        //     }
+        // }
+
+        // return false;
+
+
+        // T.C => O(logN)
+        
         int n = nums.size();
-        int l = 0;
-        int r = n-1;
+        int l = 0, r = n-1;
 
         while(l <= r) {
 
@@ -14,11 +38,27 @@ public:
 
             if(nums[mid] == target) {
                 return true;
-            } 
-            else if(nums[mid] < target) {
-                l = mid+1;
-            } else {
-                r = mid-1;
+            }
+            
+            if(nums[l] == nums[mid] && nums[mid] == nums[r]) {
+                l = l+1;
+                r = r-1;
+                continue;
+            }
+
+            if(nums[l] <= nums[mid]) {
+                if(nums[l] <= target && target <= nums[mid]) {
+                    r = mid-1;
+                } else {
+                    l = mid+1;
+                }
+            }
+            else {
+                if(nums[mid] <= target && target <= nums[r]) {
+                    l = mid+1;
+                } else {
+                    r = mid-1;
+                }
             }
         }
 
