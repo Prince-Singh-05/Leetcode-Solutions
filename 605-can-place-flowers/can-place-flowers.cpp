df00ccbecb739428ce2b90b612d095d29d41086d
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2025-08-17 09:59:29 +0530

    Time: 4 ms (6.88%) | Memory: 25.7 MB (5.81%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,57 +1,42 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-
         int len = flowerbed.size();
+        // int canBePlanted = false;
+        vector<int> temp = flowerbed;
 
-        
+        if(len == 1 && temp[0] != 1) {
+            return true;
+        }
 
         for(int i = 0; i<len; i++) {
-            if(n > 0) {
-
-                // edge case
-                if(len == 1) {
-                    if(flowerbed[0] == 0) {
-                        flowerbed[0] == 1;
-                        --n;
+            if(temp[i] == 0) {
+                if (i == 0) {
+                    if(temp[i+1] != 1) {
+                        temp[i] = 1;
+                        n--;
                     }
                 }
 
-                // end points
-                else if(i == 0 && flowerbed[i] == 0) {
-
-                    if(flowerbed[i+1] == 0) {
-                        flowerbed[i] = 1;
-                        --n;
+                if(i > 0 && i < len-1) {
+                    if(temp[i-1] != 1 && temp[i+1] != 1) {
+                        temp[i] = 1;
+                        n--;
                     }
                 }
 
-                else if(i == len-1 && flowerbed[i] == 0) {
-
-                    if(flowerbed[i-1] == 0) {
-                        flowerbed[i] = 1;
-                        --n;
-                    }
-                }
-
-                // remaining cases
-                else {
-                    if(flowerbed[i] == 0) {
-
-                        if(flowerbed[i-1] == 0 && flowerbed[i+1] == 0) {
-                            flowerbed[i] = 1;
-                            --n;
-                        }
+                if(i == len-1) {
+                    if(temp[i-1] != 1) {
+                        temp[i] = 1;
+                        n--;
                     }
                 }
             }
         }
 
-        if(n > 0) {
-            return false;
-        }
+        if(n > 0) return false;
 
         return true;
-        
+
     }
 };
