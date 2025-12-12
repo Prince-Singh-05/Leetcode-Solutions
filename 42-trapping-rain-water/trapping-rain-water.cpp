851771e487740b0dc26d9b4cf6c74ecdc4263c55
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2025-12-12 20:06:40 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (7.67%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-
         int n = height.size();
-        if(n == 1) return 0;
 
-        int ans = 0;
+        if (n == 1) return 0;
+
+        int maxi = 0;
+        int area;
         vector<int> leftToRight(n);
         vector<int> rightToLeft(n);
 
-        int maxi = 0;
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i<n; i++) {
             maxi = max(maxi, height[i]);
             leftToRight[i] = maxi;
         }
 
         maxi = 0;
-        for(int i = n-1; i >= 0; i--) {
+
+        for (int i = n-1; i >= 0; i--) {
             maxi = max(maxi, height[i]);
             rightToLeft[i] = maxi;
         }
 
-        for(int i = 0; i < n; i++) {
-            ans += min(leftToRight[i], rightToLeft[i]) - height[i];
+        for (int i = 0; i < n; i++) {
+            area += min(leftToRight[i], rightToLeft[i]) - height[i];
         }
-        
-        return ans;
+
+        return area;
     }
 };
