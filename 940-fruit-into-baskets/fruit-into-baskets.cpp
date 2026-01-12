74a0a4191af29ec4cf697b1095cf1d8c37edde2b
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-01-12 23:45:42 +0530

    Time: 59 ms (8.13%) | Memory: 84.8 MB (81.57%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int left = 0;
+        int right = 0;
+        int max_fruits = INT_MIN;
+        unordered_map<int,int> freq;
+
+        while (right < fruits.size()) {
+            freq[fruits[right]]++;
+
+            while (freq.size() > 2) {
+                freq[fruits[left]]--;
+                if (freq[fruits[left]] == 0) freq.erase(fruits[left]);
+                left++;
+            }
+
+            if (freq.size() <= 2) {
+                max_fruits = max(max_fruits, right-left+1);
+            }
+
+            right++;
+        }
+
+        return max_fruits;
+    }
+};
