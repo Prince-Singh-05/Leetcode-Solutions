bcdc6dafdf74660a9fdcd68e8e1206450802011b
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-01-14 21:49:15 +0530

    Time: 7 ms (9.01%) | Memory: 69.4 MB (90.71%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,78 +1,27 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        
-        // Brute Force => T.C = O(N^2) && S.C = O(1)
-        // int maxLen = 0;
-        // int zeros = 0;
+        int low = 0;
+        int freq[2];
+        int max_len = INT_MIN;
 
-        // for(int i = 0; i<nums.size(); i++) {
-        //     zeros = 0;
+        for (int high = 0; high < nums.size(); high++) {
+            freq[nums[high]]++;
 
-        //     for(int j = 0; j < nums.size(); j++) {
-        //         if(nums[j] == 0) {
-        //             zeros++;
-        //         }
+            int len = high-low+1;
 
-        //         if(zeros <= k) {
-        //             int len = j - i + 1;
-        //             maxLen = max(maxLen, len);
-        //         } else {
-        //             break;
-        //         }
+            while (freq[0] > k) {
+                freq[nums[low]]--;
+                low++;
 
-        //     }
-        // }
-
-        // return maxLen;
-
-        // Better => T.C = O(2N) && S.C = O(1)
-        // int maxLen = 0;
-        // int l = 0, r = 0;
-        // int zeros = 0;
-
-        // while(r < nums.size()) {
-        //     if(nums[r] == 0) {
-        //         zeros++;
-        //     }
-
-        //     while(zeros > k) {
-        //         if(nums[l] == 0) {
-        //             zeros--;
-        //         }
-        //         l++;
-        //     }
-
-        //     if(zeros <= k) {
-        //         maxLen = max(maxLen, r-l+1);
-        //     }
-        //     r++;
-        // }
-
-        // return maxLen;
-
-        // Optimal => T.C = O(N) && S.C = O(1)
-        int maxLen = 0;
-        int l = 0, r = 0;
-        int zeros = 0;
-
-        while(r < nums.size()) {
-            if(nums[r] == 0) {
-                zeros++;
             }
 
-            if(zeros > k) {
-                if(nums[l] == 0) {
-                    zeros--;
-                }
-                l++;
-            } else {
-                maxLen = max(maxLen, r-l+1);
+            if (freq[0] <= k) {
+                len = high-low+1;
+                max_len = max(max_len, len);
             }
-
-            r++;
         }
 
-        return maxLen;
+        return max_len;
     }
 };
