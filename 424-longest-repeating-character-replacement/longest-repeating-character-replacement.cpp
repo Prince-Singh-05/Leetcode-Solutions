7c5222befe80063134d853da5ece2b3c69755911
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-01-14 20:11:58 +0530

    Time: 14 ms (20.57%) | Memory: 10.9 MB (39.13%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,27 +1,41 @@
 class Solution {
-public:
-    int characterReplacement(string s, int k) {
-        
-        vector<int> freq(26);
-        int left = 0;
-        int right = 0;
+private:
+    int findMax(int *arr) {
+        int maxi = INT_MIN;
 
-        int res = 0;
-        int maxCount = 0;
-
-        while(right < s.size()) {
-            freq[s[right] - 'A']++;
-            maxCount = max(maxCount, freq[s[right] - 'A']);
-
-            if( right - left + 1 - maxCount > k) {
-                freq[s[left] - 'A']--;
-                left++;
-            }
-
-            res = max(res, right - left + 1);
-            right++;
+        for (int i = 0; i < 256; i++) {
+            maxi = max(maxi, arr[i]);
         }
 
-        return res;
+        return maxi;
+    }
+
+public:
+    int characterReplacement(string s, int k) {
+        int low = 0;
+        int freq[256];
+        int max_len = INT_MIN;
+
+        for (int high = 0; high < s.size(); high++) {
+            freq[s[high]]++;
+
+            int max_count = findMax(freq);
+            int len = high-low+1;
+            int diff = len - max_count;
+
+            while (diff > k) {
+                freq[s[low]]--;
+                low++;
+                max_count = findMax(freq);
+                len = high-low+1;
+                diff = len - max_count;
+            }
+
+            if (diff <= k) {
+                max_len = max(max_len, len);
+            }
+        }
+
+        return max_len;
     }
 };
