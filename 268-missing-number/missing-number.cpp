0284049c8db5818702775d1c6ea1442021600d7b
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-01-28 23:25:06 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (6.77%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> arr(n+1, -1);
 
-    int sortingMethod(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i<nums.size(); i++) {
-            if(nums[i] != i) {
+        for (int i = 0; i<n; i++) {
+            arr[nums[i]] = nums[i];
+        }
+
+        for (int i = 0; i<n+1; i++) {
+            if (arr[i] != i) {
                 return i;
             }
         }
 
-        return nums.size();
-    }
-
-    int missingNumber(vector<int>& nums) {
-
-        // return sortingMethod(nums);
-
-        // XOR Method
-        int ans = 0;
-        int n = nums.size();
-        for(int i = 0; i<n; i++) {
-            ans ^= nums[i];
-        }
-
-        for(int i = 0; i<=n; i++) {
-            ans ^= i;
-        }
-
-        return ans;
+        return -1;
     }
 };
