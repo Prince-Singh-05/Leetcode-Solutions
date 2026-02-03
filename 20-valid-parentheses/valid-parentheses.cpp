540e0be21e5a2a67e1c414a1d8d6203d3378a70f
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-02-03 08:43:51 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (38.10%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
-        stack<char> st;
-        int n = s.length()-1;
+        stack<char> brackets;
+        int i = 0;        
 
-        for(auto i:s)
-        {
-            if(i=='(' || i=='{' || i=='[') st.push(i);
-            else {
-                if(st.empty() || 
-                    (st.top()=='(' and i!=')') || 
-                    (st.top()=='{' and i!='}') || 
-                    (st.top()=='[' and i!=']')) 
-                    return false;
+        while (i < s.size()) {
 
-                st.pop();
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                brackets.push(s[i]);
+                i++;
+                continue;
             }
+
+            if (brackets.empty()) {
+                return false;
+            }
+            
+            if (brackets.top() == '(') {
+                if (s[i] == ')')
+                    brackets.pop();
+                else return false;
+            }
+            else if (brackets.top() == '{') {
+                if (s[i] == '}')
+                    brackets.pop();
+                else return false;
+            }
+            else if (brackets.top() == '[') {
+                if (s[i] == ']')
+                    brackets.pop();
+                else return false;
+            }
+            i++;
         }
 
-        return st.empty();
+        return brackets.empty();
     }
 };
