3c70ba3232d9ffb37b411f4e83c4a059585d39aa
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-02-07 09:07:54 +0530

    Time: 4 ms (24.28%) | Memory: 12.9 MB (35.88%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,28 +3,34 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        vector<int> ans;
+        vector<int> temp(m);
+        stack<int> st;
+        st.push(nums2[m-1]);
+        temp[m-1] = -1;
         unordered_map<int, int> map;
+        map[nums2[m-1]] = m-1;
 
-        for (int i = 0; i<m; i++) {
-            map.insert({nums2[i], i});
+        for (int i = m-2; i >= 0; i--) {
+            map[nums2[i]] = i;
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            if (st.empty()) {
+                temp[i] = -1;
+            } else {
+                temp[i] = st.top();
+            }
+
+            st.push(nums2[i]);
         }
 
-        for (int i = 0; i<n; i++) {
-            int j = map[nums1[i]];
-            j++;
-            
-            while (j < m) {
-                if (nums2[j] > nums1[i]) {
-                    ans.push_back(nums2[j]);
-                    break;
-                }
+        vector<int> ans(n);
 
-                j++;
+        for (int i = 0; i<n; i++) {
+            if (map.find(nums1[i]) != map.end()) {
+                ans[i] = temp[map[nums1[i]]];
             }
-            
-            if (j >= m) ans.push_back(-1);
-            
         }
 
         return ans;
