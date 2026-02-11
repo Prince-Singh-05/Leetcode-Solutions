549cb14e4676d95221bb019bc865017286cd5fd3
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-02-11 12:42:20 +0530

    Time: 19 ms (25.70%) | Memory: 14.8 MB (37.93%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
-    // int firstUniqChar(string s) {
-
-    //     queue<int> q;
-    //     int freq[26] = {0};
-
-    //     for(int i = 0; i<s.length(); i++) {
-    //         char ch = s[i];
-
-    //         freq[ch - 'a']++;
-    //         q.push(i);
-
-
-    //         while(!q.empty()) {
-
-    //             char frontCharacter = s[q.front()];
-    //             if(freq[frontCharacter-'a'] > 1) {
-    //                 q.pop();
-    //             } else break;
-    //         }
-    //     }
-        
-    //     if(q.empty()) {
-    //         return -1;
-    //     }
-    //     return q.front();
-
-    // }
-
     int firstUniqChar(string s) {
-        unordered_map<char, int> mp;
+        unordered_map<char, int> freq;
 
-        for(int i = 0; i<s.length(); i++) {
-            mp[s[i]]++;
+        for (char c: s) {
+            if (freq.find(c) == freq.end()) {
+                freq.insert({c, 1});
+            } else {
+                freq[c]++;
+            }
         }
 
-        for(int i = 0; i<s.length(); i++) {
-            if(mp[s[i]] == 1) 
+        for (int i = 0; i<s.size(); i++) {
+            if (freq[s[i]] == 1) {
                 return i;
+            }
         }
 
         return -1;
