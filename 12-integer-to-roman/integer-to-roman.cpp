ca353ae66b3476274e59c2689ee6161491a3bc92
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-02-18 00:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (85.67%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string romanRep[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        string ans;
+
+        int i = 0;
+        while(num > 0) {
+            if(num >= values[i]) {
+                ans += romanRep[i];
+                num -= values[i];
+            } else {
+                i++;
+            }
+        }
+
+        return ans;
+    }
+};
