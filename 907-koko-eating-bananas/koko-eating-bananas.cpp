30d74a4b52cf0c36ff13feba00f531cc36ec62e6
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-02-24 23:09:11 +0530

    Time: 3 ms (94.97%) | Memory: 23 MB (12.15%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,47 +1,37 @@
 class Solution {
+private:
+    long long hoursTaken(vector<int>& piles, int res) {
+        long long hours = 0;
+        for (int i = 0; i<piles.size(); i++) {
+            hours += (piles[i]+res-1)/res;
+        }
+
+        return hours;
+    }
+
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        
-        int left = 1;
-        int right = INT_MIN;
+        int mini = 1;
+        int maxi = INT_MIN;
+        int n = piles.size();
 
-        for(int i = 0; i<piles.size(); i++) {
-            right = max(right, piles[i]);
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, piles[i]);
         }
 
-        int ans = right;
-        while(left <= right) {
-            int mid = left + (right-left)/2;
-            long long hoursForMid = calculateHours(piles, mid);
+        int ans = maxi;
+        while (mini <= maxi) {
+            int res = mini + (maxi-mini)/2;
+            long long hours_taken = hoursTaken(piles, res);
 
-            if(hoursForMid > h) {
-                left = mid+1;
+            if (hours_taken <= h) {
+                ans = min(ans, res);
+                maxi = res-1;
             } else {
-                ans = min(ans, mid);
-                right = mid-1;
-            }
-        }
-        
-        return ans;
-    }
-
-private:
-    long long calculateHours(vector<int>& piles, int mid) {
-
-        long long totalHours = 0;
-
-        for(int i = 0; i<piles.size(); i++) {
-
-            int hoursToEatPile = piles[i]/(double)mid;
-
-            if(piles[i]%mid == 0) {
-                totalHours += hoursToEatPile;
-            } else {
-                totalHours += hoursToEatPile + 1;
+                mini = res+1;
             }
         }
 
-        return totalHours;
+        return maxi+1;
     }
-
 };
