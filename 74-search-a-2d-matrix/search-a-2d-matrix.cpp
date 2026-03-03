3491be742c90cc3a3ce5d64b028f12ad95677238
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-03-03 22:33:59 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (78.06%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -6,24 +6,17 @@ public:
 
         if (target < matrix[0][0] || target > matrix[m-1][n-1]) return false;
 
-        int row = 0;
-
-        for (int i = 0; i < m; i++) {
-            if (target <= matrix[i][n-1]) {
-                row = i;
-                break;
-            }
-        }
-
         int s = 0;
-        int e = n-1;
+        int e = (m*n)-1;
 
         while (s <= e) {
             int mid = s + (e-s)/2;
+            int row = mid/n;
+            int col = mid%n;
 
-            if (matrix[row][mid] == target) {
+            if (matrix[row][col] == target) {
                 return true;
-            } else if (matrix[row][mid] < target) {
+            } else if (matrix[row][col] < target) {
                 s = mid+1;
             } else {
                 e = mid-1;
