4687bb1a4f65757a60a6ed97df850ff6ae1ce981
Prince Singh <83657758+Prince-Singh-05@users.noreply.github.com>
2026-09-03 21:29:43 +0530

    Time: 3 ms (45.24%) | Memory: 9.8 MB (49.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        if (s.length() != t.length()) return false;
+        if (s.length() != t.length())
+            return false;
 
         unordered_map<char, int> fs;
         unordered_map<char, int> ft;
 
-        for (int i = 0; i<s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             fs[s[i]]++;
             ft[t[i]]++;
         }
 
-        for (int i = 0; i<fs.size(); i++) {
-            if (fs[i] != ft[i]) return false;
+        for (auto& pair : fs) {
+            if (pair.second != ft[pair.first]) {
+                return false;
+            }
         }
 
         return true;
